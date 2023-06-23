3d0fea4edc34c9360b7d4185ae7978eb52233360
Marcelo-Drummond-Fonseca <48736981+Marcelo-Drummond-Fonseca@users.noreply.github.com>
2023-06-22 21:18:22 -0300

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -1,84 +1,172 @@
+import pickle
 import rpyc
+from servico import Content
+import threading
+from queue import Queue
 
-class Client:
-    def __init__(self, server_address, server_port, user_id):
-        self.client = rpyc.connect(server_address, server_port).root
+class Publisher:
+    def __init__(self, server, user_id):
+        self.server = server
         self.user_id = user_id
 
-    def login(self):
-        result = self.client.exposed_login(self.user_id)
-        if result:
-            print("Login successful.")
-        else:
-            print("Login failed.")
+    def publish(self, topic, content):
+        return self.server.exposed_publish(self.user_id, topic, content)
 
-    def publish(self):
-        topic = input("Enter the topic to publish: ")
-        content = input("Enter the content: ")
-        result = self.client.exposed_publish(self.user_id, topic, content)
-        if result:
-            print("Ad published successfully.")
-        else:
-            print("Failed to publish ad.")
+    def notification_callback(self, contents):
+        # This method will not be used in the Publisher class
+        pass
 
-    def subscribe(self):
-        topic = input("Enter the topic to subscribe: ")
-        result = self.client.exposed_subscribe_to(self.user_id, topic, self.notification_callback)
-        if result:
-            print("Subscribed to topic successfully.")
-        else:
-            print("Failed to subscribe to topic.")
 
-    def unsubscribe(self):
-        topic = input("Enter the topic to unsubscribe: ")
-        result = self.client.exposed_unsubscribe_to(self.user_id, topic)
-        if result:
-            print("Unsubscribed from topic successfully.")
-        else:
-            print("Failed to unsubscribe from topic.")
+class Subscriber(threading.Thread):
+    def __init__(self, server, user_id):
+        threading.Thread.__init__(self)
+        self.server = server
+        self.user_id = user_id
+        self.ads_queue = Queue()
+
+    def run(self):
+        # No need to log in here since it's already done by the client
+        pass
+
+    def subscribe_to(self, topic):
+        return self.server.exposed_subscribe_to(self.user_id, topic)
+
+    def unsubscribe_to(self, topic):
+        return self.server.exposed_unsubscribe_to(self.user_id, topic)
 
     def list_topics(self):
-        topics = self.client.exposed_list_topics()
-        print("Available topics:", topics)
+        return self.server.exposed_list_topics()
+
+    def show_ads(self):
+        if self.ads_queue.empty() == False:
+            while self.ads_queue.empty() == False:
+                ad = self.ads_queue.get()
+                print(f"Received notification: Topic='{ad.topic}', Author='{ad.author}', Data='{ad.data}'")
+        else:
+            print("No new notifications")
 
     def notification_callback(self, contents):
         for content in contents:
-            print(f"Received notification: Topic='{content.topic}', Author='{content.author}', Data='{content.data}'")
+            deserialized_content = pickle.loads(content)
+            if isinstance(deserialized_content, Content):
+                self.ads_queue.put(deserialized_content)
+            else:
+                print("Invalid content object")
+
+
+class Client:
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.user_id = None
+        self.publisher = None
+        self.subscriber = None
+        self.connection = None
+
+    def login(self):
+        self.user_id = input("Enter your user ID: ")
+
+        self.connection = rpyc.connect(self.server_address, self.server_port)
+        self.publisher = Publisher(self.connection.root, self.user_id)
+        self.subscriber = Subscriber(self.connection.root, self.user_id)
+        self.subscriber.start()
+
+        result = self.connection.root.exposed_login(self.user_id, self.subscriber.notification_callback)
+        if result:
+            print("Login successful.")
+            return True
+        else:
+            print("Login failed.")
+            return False
+
+    def publish(self, topic, content):
+        if self.publisher:
+            result = self.publisher.publish(topic, content)
+            if result:
+                print("Ad published successfully.")
+            else:
+                print("Failed to publish ad.")
+        else:
+            print("Not logged in.")
+
+    def subscribe_to(self, topic):
+        if self.subscriber:
+            result = self.subscriber.subscribe_to(topic)
+            if result:
+                print("Subscribed to topic successfully.")
+            else:
+                print("Failed to subscribe to topic.")
+        else:
+            print("Not logged in.")
+
+    def unsubscribe_to(self, topic):
+        if self.subscriber:
+            result = self.subscriber.unsubscribe_to(topic)
+            if result:
+                print("Unsubscribed from topic successfully.")
+            else:
+                print("Failed to unsubscribe from topic.")
+        else:
+            print("Not logged in.")
+
+    def list_topics(self):
+        if self.subscriber:
+            topics = self.subscriber.list_topics()
+            print("Available topics:", topics)
+        else:
+            print("Not logged in.")
+
+    def show_ads(self):
+        if self.subscriber:
+            self.subscriber.show_ads()
+        else:
+            print("Not logged in.")
+
+    def main_menu(self):
+        while True:
+            print("\nMenu:")
+            print("1. Publish an ad")
+            print("2. Subscribe to a topic")
+            print("3. Unsubscribe from a topic")
+            print("4. List topics")
+            print("5. Show ads")
+            print("6. Exit")
+
+            choice = input("Enter your choice (1-6): ")
+
+            if choice == "1":
+                topic = input("Enter the topic to publish: ")
+                content = input("Enter the content: ")
+                self.publish(topic, content)
+            elif choice == "2":
+                topic = input("Enter the topic to subscribe: ")
+                self.subscribe_to(topic)
+            elif choice == "3":
+                topic = input("Enter the topic to unsubscribe: ")
+                self.unsubscribe_to(topic)
+            elif choice == "4":
+                self.list_topics()
+            elif choice == "5":
+                self.show_ads()
+            elif choice == "6":
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+        # Signal the end of processing ads
+        if self.subscriber:
+            self.subscriber.ads_queue.put("END")
+            self.subscriber.join()
+
+        print("Exiting...")
 
 def main():
     server_address = "localhost"  # Update with the actual server address
     server_port = 12345  # Update with the actual server port
-    user_id = input("Enter your user ID: ")
 
-    client = Client(server_address, server_port, user_id)
-
-    # Login
-    client.login()
-
-    while True:
-        print("\nMenu:")
-        print("1. Publish an ad")
-        print("2. Subscribe to a topic")
-        print("3. Unsubscribe from a topic")
-        print("4. List topics")
-        print("5. Exit")
-
-        choice = input("Enter your choice (1-5): ")
-
-        if choice == "1":
-            client.publish()
-        elif choice == "2":
-            client.subscribe()
-        elif choice == "3":
-            client.unsubscribe()
-        elif choice == "4":
-            client.list_topics()
-        elif choice == "5":
-            break
-        else:
-            print("Invalid choice. Please try again.")
-
-    print("Exiting...")
+    client = Client(server_address, server_port)
+    if client.login():
+        client.main_menu()
 
 if __name__ == "__main__":
     main()
--- servico.py
+++ servico.py
@@ -1,11 +1,13 @@
 import rpyc
 import threading
+import pickle
 from rpyc.utils.server import ThreadedServer
-from typing import Callable, List
+from typing import Callable, List, TypeAlias
 from dataclasses import dataclass
+from queue import Queue
 
-UserId = str
-Topic = str
+UserId: TypeAlias = str
+Topic: TypeAlias = str
 
 @dataclass(frozen=True, kw_only=True, slots=True)
 class Content:
@@ -13,13 +15,14 @@ class Content:
     topic: Topic
     data: str
 
-FnNotify = Callable[[List[Content]], None]
+FnNotify: TypeAlias = Callable[[List[Content]], None]
 
 class BrokerGlobals:
     topics = {}
     subscribers = {}
     admin_id = "admin"
     connections = {}
+    user_callbacks = {}
 
 class BrokerService(rpyc.Service):
     def create_topic(self, user_id: UserId, topic_name: str) -> Topic:
@@ -30,11 +33,12 @@ class BrokerService(rpyc.Service):
         BrokerGlobals.topics[topic_name] = []
         return f"Topic '{topic_name}' created successfully."
 
-    def exposed_login(self, user_id: UserId) -> bool:
+    def exposed_login(self, user_id: UserId, callback: FnNotify) -> bool:
         if user_id in BrokerGlobals.subscribers:
             return False
         BrokerGlobals.subscribers[user_id] = []
         BrokerGlobals.connections[user_id] = self._conn
+        BrokerGlobals.user_callbacks[user_id] = callback
         return True
 
     def exposed_list_topics(self) -> List[Topic]:
@@ -44,21 +48,22 @@ class BrokerService(rpyc.Service):
         if topic not in BrokerGlobals.topics:
             return False
         content = Content(author=user_id, topic=topic, data=data)
-        BrokerGlobals.topics[topic].append(content)
-        self._notify_subscribers(topic, [content])
+        serialized_content = pickle.dumps(content)
+        BrokerGlobals.topics[topic].append(serialized_content)
+        self._notify_subscribers(topic, [serialized_content])
         return True
 
-    def exposed_subscribe_to(self, user_id: UserId, topic: Topic, callback: FnNotify) -> bool:
+    def exposed_subscribe_to(self, user_id: UserId, topic: Topic) -> bool:
         if topic not in BrokerGlobals.topics:
             return False
-        BrokerGlobals.subscribers[user_id].append((topic, callback))
-        self._send_previous_ads(user_id, topic, callback)
+        BrokerGlobals.subscribers[user_id].append(topic)
+        self._send_previous_ads(user_id, topic)
         return True
 
     def exposed_unsubscribe_to(self, user_id: UserId, topic: Topic) -> bool:
         if topic not in BrokerGlobals.topics:
             return False
-        BrokerGlobals.subscribers[user_id] = [(t, cb) for t, cb in BrokerGlobals.subscribers[user_id] if t != topic]
+        BrokerGlobals.subscribers[user_id] = [t for t in BrokerGlobals.subscribers[user_id] if t != topic]
         return True
 
     def on_connect(self, conn):
@@ -68,24 +73,31 @@ class BrokerService(rpyc.Service):
         user_id = next((user_id for user_id, c in BrokerGlobals.connections.items() if c == conn), None)
         if user_id:
             del BrokerGlobals.connections[user_id]
+            del BrokerGlobals.user_callbacks[user_id]
 
     def _notify_subscribers(self, topic: Topic, contents: List[Content]) -> None:
         for user_id, subscriptions in BrokerGlobals.subscribers.items():
             conn = BrokerGlobals.connections.get(user_id)
             if conn:
-                for t, callback in subscriptions:
-                    if t == topic:
+                for sub in subscriptions:
+                    if sub == topic:
+                        callback = BrokerGlobals.user_callbacks[user_id]
                         callback(contents)
 
-    def _send_previous_ads(self, user_id: UserId, topic: Topic, callback: FnNotify) -> None:
+    def _send_previous_ads(self, user_id: UserId, topic: Topic) -> None:
         if topic in BrokerGlobals.topics:
-            callback(BrokerGlobals.topics[topic])
+            previous_ads = []
+            for content in BrokerGlobals.topics[topic]:
+                previous_ads.append(content)
+            if previous_ads:
+                callback = BrokerGlobals.user_callbacks[user_id]
+                callback(previous_ads)
 
     @staticmethod
     def start_console_input():
         broker_service = BrokerService()
         while True:
-            user_input = input("Enter 'create_topic' command: ")
+            user_input = input("Enter 'create_topic' command followed by the topic to be created: ")
             if user_input.strip() == "exit":
                 break
             broker_service.handle_console_input(user_input)
@@ -94,13 +106,13 @@ class BrokerService(rpyc.Service):
         command, *args = user_input.split()
         if command == "create_topic":
             self.create_topic(BrokerGlobals.admin_id, *args)
-
+            
 if __name__ == '__main__':
     server = ThreadedServer(BrokerService, port=12345)
     server_thread = threading.Thread(target=server.start)
     # Start the server thread
     server_thread.start()
-
+    
     # Start the console input loop
     threading.Thread(target=BrokerService.start_console_input).start()
 
